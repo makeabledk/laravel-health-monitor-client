8e78acb6625c9e12cb5fe13e852b7e2dbf6d8ebf
Rasmus Nielsen <rasmus@easify.dk>
2020-09-11 16:59:42 +0200

    Support laravel 8

--- tests/Command/InstallCommandTest.php
+++ tests/Command/InstallCommandTest.php
@@ -7,23 +7,13 @@ use Makeable\HealthMonitorClient\Tests\TestCase;
 
 class InstallCommandTest extends TestCase
 {
-    /**
-     * @throws \Throwable
-     */
-    public function tearDown(): void
-    {
-        parent::tearDown();
-
-        unlink(app()->environmentFilePath());
-    }
-
     /** @test */
     public function it_install_a_token_and_publish_config_files()
     {
-        Artisan::call('config:clear');
+        copy($this->app->environmentFilePath().'.example', $this->app->environmentFilePath());
+
         Artisan::call('health:install');
 
-        $this->assertFileExists(app()->environmentFilePath());
         $this->assertFileExists(app()->configPath().'/monitor.php');
         $this->assertFileExists(app()->configPath().'/health/config.php');
         $this->assertIsString(config('monitor.token'));
